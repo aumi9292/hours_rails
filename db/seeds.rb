96a6e34edc85bdbc02e16c2b719722b5afba1b05
Austin Miller <austinmiller@ip-192-168-1-229.us-west-2.compute.internal>
2021-10-27 11:02:40 -0600

    removed trailing commas in hashes, added hash spacing

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,27 +1,27 @@
 employees = [
-  {full_name: 'Austin Miller'}, 
-  {full_name: 'Frito Lays'}, 
-  {full_name: 'Jimmy Johns'}, 
-  {full_name: 'Lisa Loops'}
+  { full_name: 'Austin Miller' }, 
+  { full_name: 'Frito Lays' }, 
+  { full_name: 'Jimmy Johns' }, 
+  { full_name: 'Lisa Loops' }
 ]
 
 pay_periods = [
- {start_date: '2021-09-01', end_date: '2021-09-14'},
- {start_date: '2021-09-15', end_date: '2021-09-30'},
- {start_date: '2021-10-01', end_date: '2021-10-14'},
- {start_date: '2021-10-15', end_date: '2021-10-31'},
- {start_date: '2021-11-01', end_date: '2021-11-14'},
- {start_date: '2021-11-15', end_date: '2021-11-30'},
- {start_date: '2021-12-01', end_date: '2021-12-14'},
- {start_date: '2021-12-15', end_date: '2021-12-31'},
- {start_date: '2022-01-01', end_date: '2022-01-14'},
- {start_date: '2022-01-15', end_date: '2022-01-31'},
+ { start_date: '2021-09-01', end_date: '2021-09-14' },
+ { start_date: '2021-09-15', end_date: '2021-09-30' },
+ { start_date: '2021-10-01', end_date: '2021-10-14' },
+ { start_date: '2021-10-15', end_date: '2021-10-31' },
+ { start_date: '2021-11-01', end_date: '2021-11-14' },
+ { start_date: '2021-11-15', end_date: '2021-11-30' },
+ { start_date: '2021-12-01', end_date: '2021-12-14' },
+ { start_date: '2021-12-15', end_date: '2021-12-31' },
+ { start_date: '2022-01-01', end_date: '2022-01-14' },
+ { start_date: '2022-01-15', end_date: '2022-01-31' }
 ]
 
 date_hours = [
-  {date: '2021-10-26', hours: 4, employee_id: 1, pay_period_id: 4},
-  {date: '2021-10-27', hours: 1, employee_id: 2, pay_period_id: 4},
-  {date: '2021-11-08', hours: 1.5, employee_id: 3, pay_period_id: 5},
+  { date: '2021-10-26', hours: 4, employee_id: 1, pay_period_id: 4 },
+  { date: '2021-10-27', hours: 1, employee_id: 2, pay_period_id: 4 },
+  { date: '2021-11-08', hours: 1.5, employee_id: 3, pay_period_id: 5 }
 ]
 
 seed_data = [
