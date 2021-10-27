d6adb4aac02c37a8eac09db33b42d5822988dac9
Austin Miller <austinmiller@ip-192-168-1-229.us-west-2.compute.internal>
2021-10-27 10:25:03 -0600

    updated seed data input and added clean command line output for `db:seed` command

--- README.md
+++ README.md
@@ -5,6 +5,7 @@ This project is a translation of a Sinatra application server to a Rails applica
 
 ## Drafting
 ### Routes
+```ruby
 resources :employees do
   resources :pay_periods do 
     resources :date_hours
@@ -19,8 +20,10 @@ end
 'employees#create'
 'employees#update'
 'employees#destroy'
+```
 
 ### Controllers
+```ruby
 class EmployeesController < ActionController
   index
   show
@@ -50,22 +53,30 @@ class DateHoursController < ActionController
   update
   destroy
 end
+```
 
 ### Views
 Should this Rails app be concerned with any views? No, just render json data in the browser
 
 ### Models
-class EmployeesController < ActionController
+```ruby
+class Employee < ApplicationRecord
   has_many :pay_periods
-  has_many :date_hours, through: :pay_periods
+  has_many :date_hours
 end
 
-class PayPeriodsController < ActionController
+class PayPeriod < ApplicationRecord
   has_many :date_hours
   has_many :employees
 end
 
-class DateHoursController < ActionController
+class DateHour < ApplicationRecord
   belongs_to :pay_period
   belongs_to :employee
 end
+```
+
+## Notes
+1. I originally included `through: :pay_periods` in the `has_many :date_hours` invocation in the `Employee` model, but in the Rails console, when I tried to chain `date_hours` to `Employee.find(1)`, an `ActiveRecord::StatementInvalid (SQLite3::SQLException: no such column: pay_periods.employee_id)` was raised. 
+2. I noticed in the Rails console when I tried to run `PayPeriod.all` that most of the pay_periods were being returned with `nil` `start_dates` and `end_dates`. I originally seeded with a 2d array with inner arrays. I decided to switch to an array of objects, but still had the same `nil` issue. I looked more closely at the `start_date` for the `PayPeriod` with an id of 1, and saw that the month and day had been switched. So I switched the format of the seed data to match. *Look more into date parsing from strings with Rails
+3. To fully resolve the `nil` issue with `PayPeriod`s, I had to run `bundle exec rails db:reset`, not just `db:seed`. 
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,40 +6,48 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-employees = ['Austin Miller', 'Frito Lays', 'Jimmy Johns', 'Lisa Loops']
-
-employees.each do |e_full_name|
-  Employee.create(full_name: e_full_name)
-end
+employees = [
+  {full_name: 'Austin Miller'}, 
+  {full_name: 'Frito Lays'}, 
+  {full_name: 'Jimmy Johns'}, 
+  {full_name: 'Lisa Loops'}
+]
 
 pay_periods = [
- ['09/01/2021','09/14/2021'],
- ['09/15/2021','09/30/2021'],
- ['10/01/2021','10/14/2021'],
- ['10/15/2021','10/31/2021'],
- ['11/01/2021','11/14/2021'],
- ['11/15/2021','11/30/2021'],
- ['12/01/2021','12/14/2021'],
- ['12/15/2021','12/31/2021'],
- ['01/01/2022','01/14/2022'],
- ['01/15/2022','01/31/2022'],
+ {start_date: '2021-09-01', end_date: '2021-09-14'},
+ {start_date: '2021-09-15', end_date: '2021-09-30'},
+ {start_date: '2021-10-01', end_date: '2021-10-14'},
+ {start_date: '2021-10-15', end_date: '2021-10-31'},
+ {start_date: '2021-11-01', end_date: '2021-11-14'},
+ {start_date: '2021-11-15', end_date: '2021-11-30'},
+ {start_date: '2021-12-01', end_date: '2021-12-14'},
+ {start_date: '2021-12-15', end_date: '2021-12-31'},
+ {start_date: '2022-01-01', end_date: '2022-01-14'},
+ {start_date: '2022-01-15', end_date: '2022-01-31'},
 ]
-
-pay_periods.each do |start_date, end_date|
-  PayPeriod.create(start_date: start_date, end_date: end_date)
-end
 
 date_hours = [
-  ['10/26/21', 4, 1, 4],
-  ['10/27/21', 1, 2, 4],
-  ['11/08/21', 1.5, 3, 5],
+  {date: '2021-10-26', hours: 4, employee_id: 1, pay_period_id: 4},
+  {date: '2021-10-27', hours: 1, employee_id: 2, pay_period_id: 4},
+  {date: '2021-11-08', hours: 1.5, employee_id: 3, pay_period_id: 5},
 ]
 
-date_hours.each do |date, hours, employee_id, pay_period_id|
-  DateHour.create(
-    date: date, 
-    hours: hours, 
-    employee_id: employee_id, 
-    pay_period_id: pay_period_id
-  )
+seed_data = [
+  { model: Employee, data: employees },
+  { model: PayPeriod, data: pay_periods },
+  { model: DateHour, data: date_hours }
+]
+
+def seed(model, collection)
+  puts "\n#{model.name}s"
+  collection.each do |cl_hash|
+    puts cl_hash
+    model.create(cl_hash)
+  end
 end
+
+puts "\nSeeding Hours Database"
+
+seed_data.each { |set| seed(set[:model], set[:data]) }
+
+puts "\nSuccessfully seeded Hours Database"
